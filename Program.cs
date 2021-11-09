680efa8f9963ddfd021e12b47b753b42ca5bff84
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-11-09 15:22:25 +0100

    Clear the screen

--- Program.cs
+++ Program.cs
@@ -32,8 +32,8 @@ namespace SharpEngine
                 Glfw.PollEvents();
                 
                 // Set clear color
-                // glClearColor(0, 0, 0, 1);
-                // glClear(GL_COLOR_BUFFER_BIT);
+                glClearColor(0, 0, 0, 1);
+                glClear(GL_COLOR_BUFFER_BIT);
                 
                 // Draw the array:
                 glDrawArrays(GL_TRIANGLES, 0, 3);
